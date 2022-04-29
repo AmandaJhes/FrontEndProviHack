e7da9d5bac464a1f8522b2410733ad6e25888a67
Cassiano Moura <86990508+MouraCass@users.noreply.github.com>
2022-04-29 10:00:36 -0300

    Update Form.js

--- src/Components/Formulario/Form.js
+++ src/Components/Formulario/Form.js
@@ -4,7 +4,7 @@ import { Button } from "./styles"
 function MyForm() {
    return (
       <form>
-      <label>Name:<input type="text" name="nome "/>
+      <label>Name:<input type="text" name="name "/>
       </label>
       <Button>Buscar</Button>
     </form>
