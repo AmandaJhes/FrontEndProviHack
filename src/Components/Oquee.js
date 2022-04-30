8b8306fe68d6d0357449eb4f1f3402379ffaa8a6
Gabriel Mina da Silva <gabrielminadasilva@MacBook-Air-de-Gabriel.local>
2022-04-30 16:24:59 -0300

    pegando material

---
+++ src/Components/Oquee.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+
+const Main = styled.div`
+    display: flex;
+`
+
+export const Oque = () =>{
+
+    return(
+        <Main>
+            <div></div>
+            <div>
+                <h1>O que é o "nome do projeto"?</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </Main>
+    )
+}
