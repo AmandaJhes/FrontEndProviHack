eccb668d1867406abd3cab8446e38b27cd7da563
Gabriel Mina da Silva <gabrielminadasilva@MacBook-Air-de-Gabriel.local>
2022-05-01 14:24:39 -0300

    feat: Componente 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react"
 import styled from "styled-components";
 import Header from "./Components/Header/Header";
+import PlanetB from "./Components/PlanetB/PlanetB";
 // import Login from "./Components/Formulario/Login";
 import "./Styles/App.css"
+
 function App() {
   return (
     <div>
       {/* <h3><Login /> </h3> */}
       {/* <h3><Cadastro /></h3> */}
-      <Header/>
+      {/* <Header/> */}
+      {/* <PlanetB/> */}
     </div>
   );
 }
---
+++ src/Components/PlanetB/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Main = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width:100%;
+height:100vh;
+background-color: #0b0b11;
+`
+
+export const MainPic = styled.img`
+width:80%;
+height:90%;
+display:flex;
+align-items: center;
+justify-content: flex-end;
+`
